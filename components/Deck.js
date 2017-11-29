41c3330fae8b21d28114c9db17b47f4f33ad6cb3
Laszlo Suslecz <drsuslac@gmail.com>
2017-11-29 15:31:38 +0100

    feat: add navigationOptions and redux connection to Deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'
+import { connect } from 'react-redux'
 
 export default class Deck extends Component {
+
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params
+
+    return {
+      title: title
+    }
+  }
+
   render() {
+
+    const { deck } = this.props
+
     return(
-      <Text>Single Deck View</Text>
+      <Text>Single Deck View - {this.props.navigation.state.params.title}</Text>
     )
   }
 }
+
+function mapStateToProps (state, { navigation }) {
+  const { title } = navigation.state.params
+
+  return {
+    title,
+    deck: decks[title]
+  }
+
+}
+
+//export default connect(mapStateToProps, nil)(Deck)
