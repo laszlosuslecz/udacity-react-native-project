02900de6fdcc86e51ed61f3e88cc504ffbf44c60
Laszlo Suslecz <drsuslac@gmail.com>
2017-11-29 15:42:13 +0100

    feat: add title to main(DeckList)

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'
 
-export default class DeckList extends React.Component {
+export default class DeckList extends Component {
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Home'
+    }
+  }
 
   goToCreateDeck = () => this.props.navigation.navigate('CreateDeck')
 
