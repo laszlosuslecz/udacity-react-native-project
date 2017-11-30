8dfb6760c8d5d973ed28f29b70db930e77e914a4
Laszlo Suslecz <drsuslac@gmail.com>
2017-11-30 06:41:16 +0100

    test: render mock data to the screen

--- components/DeckList.js
+++ components/DeckList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import { TabNavigator, StackNavigator } from 'react-navigation'
 
+import { getMockData, setupInitialResults } from '../utils/MockData'
+
 export default class DeckList extends Component {
 
   static navigationOptions = ({ navigation }) => {
@@ -13,9 +15,13 @@ export default class DeckList extends Component {
   goToCreateDeck = () => this.props.navigation.navigate('CreateDeck')
 
   render() {
+
+    const MockData = getMockData()
+    console.log(MockData)
     return(
       <View style={styles.container}>
         <Text>Decklist</Text>
+        <Text>{MockData.React.title}</Text>
         <TouchableOpacity onPress={this.goToCreateDeck}>
           <Text>Create a deck</Text>
         </TouchableOpacity>
--- utils/MockData.js
+++ utils/MockData.js
@@ -1,24 +1,40 @@
-export const MockData = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  }
+import { AsyncStorage } from 'react-native'
+import { STORAGE_KEY } from '../api'
+
+export function getMockData () {
+
+	const MockData = {
+		React: {
+			title: 'React',
+			questions: [
+				{
+					question: 'What is React?',
+					answer: 'A library for managing user interfaces'
+				},
+				{
+					question: 'Where do you make Ajax requests in React?',
+					answer: 'The componentDidMount lifecycle event'
+				}
+			]
+		},
+		JavaScript: {
+			title: 'JavaScript',
+			questions: [
+				{
+					question: 'What is a closure?',
+					answer: 'The combination of a function and the lexical environment within which that function was declared.'
+				}
+			]
+		}
+	}
+
+	AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(MockData));
+	
+	return MockData
+}
+
+export function setupInitialResults (results) {
+  return results === null
+    ? getMockDataData()
+    : JSON.parse(results);
 }
